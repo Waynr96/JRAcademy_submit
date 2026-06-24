50aea3d6225dfed646eab376e24b097132e4441d
Wayne Yang <tzc.wayne@gmail.com>
2026-06-24 23:59:52 +1000

    login

---
+++ careerMate_react/careerMate/src/pages/login/login.jsx
@@ -0,0 +1,18 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
+        <label>Email</label>
+        <input type="email" placeholder="Enter your email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" placeholder="Enter your password" />
+      </div>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
